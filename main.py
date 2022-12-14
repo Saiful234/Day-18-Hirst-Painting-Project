ee12c5b0c43b42ea8e662fcf740cb7cc938af25e
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-12-14 14:53:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+# from turtle import Turtle, Screen
+# import random
+#
+# turtle = Turtle()
+# turtle.shape("turtle")
+# turtle.color("lime green")
+
+
+
+
+
+
+# for i in range(4):
+#     turtle.fd(100)
+#     turtle.rt(90)
+
+
+# tom.fd(100)
+# tom.pensize(10)
+# tom.width(10)
+
+# tom.speed(0)
+# def draw_spriograpgh(size):
+#     for i in range(int(360/size)):
+#         change_color()
+#         tom.circle(50)
+#         current_direction = tom.heading()
+#         tom.setheading(current_direction + size)
+#
+# draw_spriograpgh(50)
+# def change_color():
+#     R = random.random()
+#     G = random.random()
+#     B = random.random()
+#
+#     turtle.color(R, G, B)
+#
+# direction = [0, 90, 180, 270, 360]
+# turtle.setheading(90)
+# turtle.width(10)
+# turtle.speed(0)
+# for i in range(200):
+#     change_color()
+#     turtle.forward(30)
+#     turtle.setheading(random.choice(direction))
+
+# def draw_shape(angle):
+#     degree = 360 / angle
+#     change_color()
+#     for i in range(angle):
+#         turtle.fd(100)
+#         turtle.rt(degree)
+# for times in range(3, 11):
+#     draw_shape(times)
+t = (1, 2, 3, 5)
+t = t +(6)
+print(t)
+
+
+# screen = Screen()
+# screen.exitonclick()
+
